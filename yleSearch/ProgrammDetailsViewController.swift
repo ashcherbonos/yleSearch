7a60a943e1f4ae74d9499d8616d74b3b308d3deb
ashcherbonos <ashcherbonos@gmail.com>
2018-04-05 18:51:05 +0300

    activity Indicator added

--- yleSearch/ProgrammDetailsViewController.swift
+++ yleSearch/ProgrammDetailsViewController.swift
@@ -12,6 +12,7 @@ class ProgrammDetailsViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView! {didSet {
         if let imageURL = programm?.fullImageURL  {
+            
             let task = URLSession.shared.dataTask(with: imageURL) { data, response, error in
                 guard error == nil,
                     response.statusCodeIsOK,
@@ -19,17 +20,13 @@ class ProgrammDetailsViewController: UIViewController {
                     let image = UIImage(data: data)
                     else {
                         DispatchQueue.main.async {
-                            
-                            UIView.transition(with: self.imageView,
-                                              duration: 1,
-                                              options: [.transitionCrossDissolve],
-                                              animations: {  self.imageView.removeFromSuperview() },
-                                              completion: nil)
+                            self.activityIndicator.stopAnimating()
+                            self.imageView.removeFromSuperview()
                         }
                         return
                 }
-                
                 DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
                     self.imageView.image = image
                     self.imageView.fadeIn(in: 0.5)
                 }
@@ -42,5 +39,8 @@ class ProgrammDetailsViewController: UIViewController {
     @IBOutlet weak var typeLabel: UILabel!  {didSet { typeLabel.text = programm?.type }}
     @IBOutlet weak var dateLabel: UILabel!  {didSet { dateLabel.text = programm?.dataModified }}
     @IBOutlet weak var descriptionLabel: UILabel! {didSet { descriptionLabel.text = programm?.description }}
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     public var programm: TvProgramm? = nil
+    
 }
